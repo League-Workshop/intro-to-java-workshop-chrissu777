2b77e04cfd013cde989caa4c48adcb4b25299c4e
DrOranges6 <league@WTS-iMac-11.attlocal.net>
2019-04-10 14:25:30 -0700

    day 3
    :smile:

--- src/section4/DragonFight.java
+++ src/section4/DragonFight.java
@@ -7,59 +7,71 @@ import javax.swing.ImageIcon;
 public class DragonFight {
 	// 1. Create a main method and put all of the code below inside of it
 	public static void main(String[] args) {
-		
+		Random generator = new Random();
 		ImageIcon dragon = new ImageIcon("src/section4/dragonPicture.jpg");		
 		
 		JOptionPane.showMessageDialog(null, "Defeat the dragon to take its treasure!", "Dragon Fighter", 0, dragon);
 		// 2. Create a variable called "playerHealth" to store your health (set it equal to 100)
-	
+	int playerHealth = 100;
 		// 3. Create a variable called "dragonHealth" to store the dragon's health (set it equal to 100)
-		
+		int dragonHealth = 100;
 		// 4. Create a variable to hold the damage the player's attack does each round
-		
+		int playerdamage = 0;
 		// 5. Create a variable to hold the damage the dragon's attack does each round
-		
+		int dragondamage = 0;
 		
 		// 6.  Delete the slashes at the beginning of the next line.  
-		//while(playerHealth>0 && dragonHealth>0) {    //this line of code keeps the battle going until someone's health reaches 0 
+	while(playerHealth>0 && dragonHealth>0) {    //this line of code keeps the battle going until someone's health reaches 0 
 		
 		// 7. Add a closing mustache at the very bottom of this program (since we just added an opening mustache on the previous step).
 		
 		// 8. Ask the player in a pop-up if they want to attack the dragon with a yell or a kick
-		
+		String attack = JOptionPane.showInputDialog("Do you want to attack the dragon with a yell or a kick?");
 		// 9. If they typed in "yell":
-		
+		if(attack.equalsIgnoreCase("yell")) {
+		int damage = generator.nextInt(11);
 			//-- Find a random number between 0 and 10 and store it in dragonDamage
-			
+			dragondamage=dragondamage+damage;
 			//-- Subtract that number from the dragon's health variable 
-			
+			dragonHealth=dragonHealth-damage;
+		}
 		// 10. If they typed in "kick":
-		
+		if(attack.equalsIgnoreCase("kick")) {
+		int damage=generator.nextInt(26);
 			//-- Find a random number between 0 and 25 and store it in dragonDamage
-			
+			dragondamage=dragondamage+damage;
 			//-- Subtract that number from the dragon's health variable
-			
+			dragonHealth=dragonHealth-damage;
+		}
 			
 		
 		// 11.  Find a random number between 0 and 35 and store it in playerDamage
-		
+		int damage = generator.nextInt(35);
 		// 12. Subtract this number from the player's health
-		
+		playerHealth=playerHealth-damage;
 		
 		
 		// 13. If the user's health is less than or equal to 0
-		
+		if(playerHealth<=0) {
 			//-- Tell the user that they lost
-			
-		
+			JOptionPane.showMessageDialog(null, "You lost, you useless piece of trash.");
+		speak("You lost, you useless piece of trash.");
+		}
 		// 14. Else if the dragon's health is less than or equal to 0
-		
+		if(dragonHealth<=0) {
+			JOptionPane.showMessageDialog(null, "You won.");
+		}
 			//-- Tell the user that the dragon is dead and they took a ton of gold!
 			
 	   //  15.  Else
-			
+		else JOptionPane.showMessageDialog(null, "Your health is "+playerHealth+"\n The dragon's health is"+dragonHealth);
 			//-- Pop up a message that tells the their current health and the dragon's currently health (Bonus: Also display the amount of health that was lost for each player this round)
 			
 		}
 	}
 
+	private static void speak(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+}
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,22 +1,35 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int score = 0;
 		// 2.  Ask the user a question 
-		
+		String answer1 = JOptionPane.showInputDialog("What year is it?");
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if(answer1.equalsIgnoreCase("2019")){
+			score=score+1;
+		}
 		// 4.  if the user's answer is correct
 		
 		// -- add one to their score 
 		
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
+		String answer2 = JOptionPane.showInputDialog("What month is it?");
+		if(answer2.equalsIgnoreCase("April")) {
+			score=score+1;
+		}
+		String answer3 = JOptionPane.showInputDialog("What day is it?");
+		if(answer3.equalsIgnoreCase("Wednesday")) {
+			score=score+1;
 		
+		}
 		// 6.  After all the questions have been asked, print the user's score 
-		
+		JOptionPane.showMessageDialog(null, score);
+
 	}
 }
